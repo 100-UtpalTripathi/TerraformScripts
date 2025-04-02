527a5e677fb841aceb7a2769f320348b9f7b4340
Utpal Tripathi <utripathi@presidio.com>
2025-04-02 18:05:40 +0530

    creating s3 bucket, how to upload files from local understood

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+resource "aws_s3_bucket" "tripathi-s3-bucket" {
+  bucket = "tripathi-s3-bucket"
+  tags = {
+    Name = "tripathi-s3-bucket"
+  }
+}
+
+resource "aws_s3_object" "bucket-data" {
+  bucket = aws_s3_bucket.tripathi-s3-bucket.bucket
+  key    = "data.txt"
+  source = "./data.txt"
+}
